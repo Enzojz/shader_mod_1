3dd03ad61b774073ed159515acee6bcd017182b8
Enzojz <enzojz@gmail.com>
2023-04-09 19:31:02 +0200

    tpf2

--- strings.lua
+++ strings.lua
@@ -1,20 +1,12 @@
 descEN = [[This mod fix and add extra effects on shaders.
-- Fix the yellow cast on tunnel ceiling.
 - Enables shadow free materials
 - Enables lightmap
 - Enhanced UV projection solutions
 ]]
 descFR = [[Ce mod corrige et ajoute des effets supplémentaires sur shaders.
-- Correction de cast jeune sur les ceil des tunnels.
 - Possibilté de supprimer les ombres
 - Possibilté d'application de lightmap
 - Projection des UV en façons divers
-]]
-descCN = [[该MOD改进并且增加一些着色器的效果
-- 修正隧道顶部偏黄的问题
-- 取消阴影效果
-- 增加光照贴图支持
-- 增加多种UV投射方式
 ]]
 
 function data()
@@ -29,7 +21,21 @@ function data()
 	},
 	zh_CN = {
 		title = "着色器增强",
-        description = descCN,				
+		description = 
+[[该模组改进并且增加一些着色器的效果
+- 取消阴影效果
+- 增加光照贴图支持
+- 增加多种UV投射方式
+]]				
+	},
+	zh_TW = {
+		title = "著色器增強",
+        description = 
+[[該模組改進並且增加一些著色器的效果
+- 取消陰影效果
+- 增加光照貼圖支持
+- 增加多種UV投射方式
+]]			
 	},
 }
 end
